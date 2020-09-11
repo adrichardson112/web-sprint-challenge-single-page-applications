f4b5064d9e63553aff5f3ded42b1c0d0e706db96
Ashley Richardson <ashley-richardson@lambdastudents.com>
2020-09-11 19:45:26 -0400

    initial imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import Form from "./Form";
+import Formschema from "./Formschema";
+import * as Yup from "yup";
+
+const beginningFormValues = {
+  size: "",
+  instructions: "",
+  name: "",
+  toppings: {
+    pepperoni: false,
+    peppers: false,
+    mushrooms: false,
+    sausage: false,
+    onion: false,
+  }
+}
+
+const beginningFormErrors = {
+  size: "",
+  instructions: "",
+  name: "",
+}
 
 const App = () => {
   return (
